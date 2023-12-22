40811ffe06a7ee4aab44e09c919e030c0f7c50dd
yadevom <yaormu@gmail.com>
2023-12-22 11:51:31 -0500

    punteros

---
+++ pildora/App/35_punteros.cpp
@@ -0,0 +1,18 @@
+/*
+* Punteros
+    - Qué son los punteros
+        Son una caracteristica fundamental en C++ que permiten una gestión eficiente y detallada de memoria y otros recursos del hardware.
+        Los punteros son variables cuya función principal es almacenar direcciones de memoria.
+    - Declaración
+    - Almacenar direcciones en punteros
+    - Defering con punteros
+    - Ubicación de memoria dinámica
+    - Aritmética de punteros
+    - Punteros y arrays
+    - Paso de parámetros por referencia con punteros
+    - Constantes y punteros
+* Punteros a funcion
+* Problemas con punteros
+* Constantes y referencias
+* Punteros inteligentes
+*/
