4484a2c582a23998f0dc1a943374a05f5592b0b0
yadevom <yaormu@gmail.com>
2023-12-26 23:30:18 -0500

    punteros

--- pildora/App/35_punteros.cpp
+++ pildora/App/35_punteros.cpp
@@ -1,6 +1,8 @@
 /*
 * Punteros
-    - Qué son los punteros
+
+    - QUÉ SON
+    - Qué son los punteros?
         - Son una caracteristica fundamental en C++ que permiten una gestión eficiente y detallada de memoria y otros recursos del hardware.
         Los punteros son variables cuya función principal es almacenar direcciones de memoria.
         - ¿Que puede haber en las direcciones de memoria almacenada por los punteros?
@@ -12,6 +14,22 @@
             - Bloque de memoria dinámica
             - Bloque de memoria reservado
             - Etc
+
+    - PARA QUÉ SIRVEN
+    - ¿Para qué sirven los punteros? Principales aplicaciones
+        - Acceso directo y modificación de memoria
+        - Funciones dinámicas y estructura de datos
+        - Paso de parámetro por referencia
+        - Arrays y Strings
+        - Programación de bajo nivel
+    - PUNTEROS VS NO PUNTEROS
+        - PUNTEROS                                  - NO PUNTEROS
+        - Control director de la memoria            - Más simplicidad en el desarrollo
+        - Flexibilidad en manejo de datos           - Mayor seguridad
+        - Interacción con hardware de bajo nivel    - Menor preocupación por fallos de memoria
+        - Paso de parámetros por referencia
+        - Compatibilidad con C
+
     - Declaración
     - Almacenar direcciones en punteros
     - Defering con punteros
